33507c456e55fce9377873512fe5128cdbcf4c90
AdamCzyryca <62111741+aczy623@users.noreply.github.com>
2021-10-05 15:23:32 -0400

    test commit

---
+++ C#_Fundamentals/HelloWorld/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        /*
+            main is first method to be ran/called
+            static: dont have to instantiate program first to use that method
+
+        */
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
